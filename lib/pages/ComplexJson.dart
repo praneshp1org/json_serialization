9734d8c5323f456f9aac3c5452489770597d2339
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-23 14:51:35 +0545

    some ammendment

--- lib/pages/ComplexJson.dart
+++ lib/pages/ComplexJson.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:json_serialization/models/u_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CJSON extends StatefulWidget {
   const CJSON({Key? key}) : super(key: key);
@@ -8,15 +11,59 @@ class CJSON extends StatefulWidget {
 }
 
 class _CJSONState extends State<CJSON> {
+  var url, response;
+  List data = [];
+  List<Users1> userList = [];
+  bool _isLoading = true;
+
+  Future getData() async {
+    url = Uri.parse("https://jsonplaceholder.typicode.com/users");
+    response = await http.get(url);
+    data = jsonDecode(response.body);
+    setState(() {
+      userList = data.map((json) => Users1.fromJson(json)).toList();
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Complex JSON'),
-      ),
-      body: Center(
-        child: Text('JSON'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Complex JSON'),
+        ),
+        body: (_isLoading == true)
+            ? Center(
+                child: LinearProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: userList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 5,
+                    child: ListTile(
+                      title: Text(userList[index].username),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(userList[index].email),
+                          Text(userList[index].company.name),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(userList[index].company.catchPhrase)
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ));
   }
 }
